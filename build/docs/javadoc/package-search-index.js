a487714983cbdbd96a0bce3b603f00898d607e8f
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-03-27 22:55:33 -0400

    Remove build/

--- build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dev.creoii.greatbigworld.thealterworld"},{"l":"dev.creoii.greatbigworld.thealterworld.client"},{"l":"dev.creoii.greatbigworld.thealterworld.mixin.world"}];updateSearchResults();
