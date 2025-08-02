ccf9859a7781f3edb6e8f07b4b4e3dd7e6cbdae9
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-08-01 22:40:47 -0400

    Remove Wandering Traders from Overworld

---
+++ src/main/java/dev/creoii/greatbigworld/thealterworld/mixin/world/WanderingTraderManagerMixin.java
@@ -0,0 +1,18 @@
+package dev.creoii.greatbigworld.thealterworld.mixin.world;
+
+import dev.creoii.greatbigworld.GreatBigWorld;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.WanderingTraderManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WanderingTraderManager.class)
+public class WanderingTraderManagerMixin {
+    @Inject(method = "spawn", at = @At("HEAD"), cancellable = true)
+    private void gbw$removeWanderingTraderFromOverworld(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals, CallbackInfo ci) {
+        if (world.getRegistryKey() == GreatBigWorld.ALTERWORLD_KEY)
+            ci.cancel();
+    }
+}
