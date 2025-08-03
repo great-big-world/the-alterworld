405bf08e1f1e86cb700af45d75a89c59c9f1c504
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-08-03 15:58:00 -0400

    Move biome files from Flora & Fauna to here
    Fix Skeletons/Zombies converting to variants in Overworld

---
+++ src/main/java/dev/creoii/greatbigworld/thealterworld/mixin/entity/SkeletonEntityMixin.java
@@ -0,0 +1,21 @@
+package dev.creoii.greatbigworld.thealterworld.mixin.entity;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.AbstractSkeletonEntity;
+import net.minecraft.entity.mob.SkeletonEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(SkeletonEntity.class)
+public abstract class SkeletonEntityMixin extends AbstractSkeletonEntity {
+    protected SkeletonEntityMixin(EntityType<? extends AbstractSkeletonEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @ModifyExpressionValue(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/SkeletonEntity;isAlive()Z"))
+    private boolean gbw$disableSkeletonCovertInOverworld(boolean original) {
+        return original && getWorld().getRegistryKey() != World.OVERWORLD;
+    }
+}
---
+++ src/main/java/dev/creoii/greatbigworld/thealterworld/mixin/entity/ZombieEntityMixin.java
@@ -0,0 +1,21 @@
+package dev.creoii.greatbigworld.thealterworld.mixin.entity;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ZombieEntity.class)
+public abstract class ZombieEntityMixin extends HostileEntity {
+    protected ZombieEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @ModifyExpressionValue(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/ZombieEntity;isAlive()Z"))
+    private boolean gbw$disableSkeletonCovertInOverworld(boolean original) {
+        return original && getWorld().getRegistryKey() != World.OVERWORLD;
+    }
+}
