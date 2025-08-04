21a8b567ca9bf77d83b51a9da521198eadb23d91
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-08-03 20:14:03 -0400

    Fix Alterworld Portal spwaning players in ground

--- src/main/java/dev/creoii/greatbigworld/thealterworld/block/AlterworldPortalBlock.java
+++ src/main/java/dev/creoii/greatbigworld/thealterworld/block/AlterworldPortalBlock.java
@@ -22,7 +22,6 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.*;
-import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.tick.ScheduledTickView;
 import org.jetbrains.annotations.Nullable;
 
@@ -82,37 +81,32 @@ public class AlterworldPortalBlock extends Block implements Portal {
         if (serverWorld == null) {
             return null;
         } else {
-            WorldBorder worldBorder = serverWorld.getWorldBorder();
-            BlockPos blockPos = worldBorder.clampFloored(entity.getX(), entity.getY(), entity.getZ());
-            Vec3d vec3d = findOpenPosition(blockPos.toBottomCenterPos(), serverWorld, entity, entity.getDimensions(entity.getPose()));
-            BlockPos foundPos = new BlockPos((int) vec3d.x, (int) vec3d.y, (int) vec3d.z);
+            Vec3d vec3d = serverWorld.getWorldBorder().clampFloored(entity.getX(), entity.getY(), entity.getZ()).toBottomCenterPos();
+            BlockPos blockPos = new BlockPos((int) vec3d.x, (int) vec3d.y, (int) vec3d.z);
             if (!world.getBlockState(pos).get(FRACTURED)) {
                 Optional<AlterworldPortal> optional2 = AlterworldPortal.getNewPortal(serverWorld, pos, Direction.Axis.X);
                 optional2.ifPresent(portal -> {
                     portal.createPortal(serverWorld);
                 });
-            } else if (serverWorld.getBlockState(foundPos).isSolidBlock(serverWorld, foundPos)) {
-                EntityDimensions dimensions = entity.getDimensions(entity.getPose());
-                vec3d = BlockPos.findClosest(foundPos, 16, 64, blockPos1 -> {
-                    Box box = dimensions.getBoxAt(Vec3d.ofBottomCenter(blockPos1));
-                    return serverWorld.isSpaceEmpty(entity, box, false);
-                }).orElse(serverWorld.getTopPosition(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, foundPos)).toBottomCenterPos();
+            } else if (serverWorld.getBlockState(blockPos).isSolidBlock(serverWorld, blockPos)) {
+                Vec3d vec3d1 = findOpenPosition(serverWorld, entity, blockPos);
+                return new TeleportTarget(serverWorld, vec3d1, Vec3d.ZERO, 0f, 0f, PositionFlag.combine(PositionFlag.DELTA, PositionFlag.ROT), TeleportTarget.SEND_TRAVEL_THROUGH_PORTAL_PACKET.then(entityx -> entityx.addPortalChunkTicketAt(blockPos)));
             }
             return new TeleportTarget(serverWorld, vec3d, Vec3d.ZERO, 0f, 0f, PositionFlag.combine(PositionFlag.DELTA, PositionFlag.ROT), TeleportTarget.SEND_TRAVEL_THROUGH_PORTAL_PACKET.then(entityx -> entityx.addPortalChunkTicketAt(blockPos)));
         }
     }
 
-    public static Vec3d findOpenPosition(Vec3d fallback, ServerWorld world, Entity entity, EntityDimensions dimensions) {
-        if (!(dimensions.width() > 4f) && !(dimensions.height() > 4f)) {
-            double d = (double)dimensions.height() / (double)2f;
-            Vec3d vec3d = fallback.add(0f, d, 0f);
-            VoxelShape voxelShape = VoxelShapes.cuboid(Box.of(vec3d, dimensions.width(), 0f, dimensions.width()).stretch(0f, 1f, 0f).expand(1e-6));
-            Optional<Vec3d> optional = world.findClosestCollision(entity, voxelShape, vec3d, dimensions.width(), dimensions.height(), dimensions.width());
-            Optional<Vec3d> optional2 = optional.map((pos) -> pos.subtract(0f, d, 0f));
-            return optional2.orElse(fallback);
-        } else {
-            return fallback;
-        }
+    public static Vec3d findOpenPosition(ServerWorld world, Entity entity, BlockPos center) {
+        /*EntityDimensions dimensions = entity.getDimensions(entity.getPose());
+        if (dimensions.width() <= 4f && dimensions.height() <= 4f) {
+            for (BlockPos pos : BlockPos.iterateOutwards(center, 32, 64, 32)) {
+                Vec3d vec3d = Vec3d.ofCenter(pos);
+                if (world.isSpaceEmpty(entity, dimensions.getBoxAt(vec3d))) {
+                    return vec3d;
+                }
+            }
+        }*/
+        return world.getTopPosition(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, center).toBottomCenterPos();
     }
 
     @Override
