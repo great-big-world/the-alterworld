a41bb045155656f3a6fa1bf38658b527c39266ad
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-09-15 16:25:06 -0400

    Fix Patrols spawning in Overworld

---
+++ src/main/java/dev/creoii/greatbigworld/thealterworld/mixin/world/PatrolSpawnerMixin.java
@@ -0,0 +1,18 @@
+package dev.creoii.greatbigworld.thealterworld.mixin.world;
+
+import dev.creoii.greatbigworld.GreatBigWorld;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.spawner.PatrolSpawner;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PatrolSpawner.class)
+public class PatrolSpawnerMixin {
+    @Inject(method = "spawn", at = @At("HEAD"), cancellable = true)
+    private void gbw$cancelPatrolsInOverworld(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals, CallbackInfo ci) {
+        if (!world.getRegistryKey().equals(GreatBigWorld.ALTERWORLD_KEY))
+            ci.cancel();
+    }
+}
